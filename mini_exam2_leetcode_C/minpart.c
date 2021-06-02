84461e8d695f0aa1cbc28cebcacfa08575191f9f
Suraba03 <74858533+Suraba03@users.noreply.github.com>
2021-06-02 16:00:31 +0300

    Add files via upload

---
+++ mini_exam2_leetcode_C/minpart.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define STR_LEN 100000
+
+int minPartitions(char *n)
+{
+    int m = 0;
+    for (int i = 0; i < strlen(n); i++) {
+        if (n[i] - 48 > m) {
+            m = n[i] - 48;
+        }
+    }
+    return m;
+}
+
+int main(void)
+{
+    char str[STR_LEN];
+    printf("введите строку\n");
+    scanf("%s", str);
+    printf("минимальное кол-во слагаемых = %d\n", minPartitions(str));
+    return 0;
+}
